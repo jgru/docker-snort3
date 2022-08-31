840469678818eb97f35cae1a9754856402c8a787
Jan Gru <j4n6ru@gmail.com>
2022-08-31 11:14:56 +0200

    Add commentary to script

--- grab_rules.sh
+++ grab_rules.sh
@@ -9,11 +9,17 @@ MASTER_RULES="snort.rules"
 
 # Retrieve rules
 cd ./etc/rules/
-wget https://www.snort.org/downloads/community/$RULES_TAR
-tar xvfz $RULES_TAR
+echo "[*] Downloading ${RULES_TAR}"
+wget https://www.snort.org/downloads/community/$RULES_TAR 2> /dev/null && echo "[+] Retrieved ${RULES_TAR}"
+tar xvfz $RULES_TAR > /dev/null && echo "[+] Extracted ${RULES_TAR}"
+
 # Combine rules
 mv ${RULES_DIR}/* .
+CNT=$(wc -l ${MASTER_RULES} | cut -f1 -d' ')
 cat ${RULES_FILE} >> ${MASTER_RULES}
+echo "$(sort -u ${MASTER_RULES})" > ${MASTER_RULES}
+CNTNEW=$(wc -l ${MASTER_RULES} | cut -f1 -d' ')
+echo "[*] Added $((CNTNEW - CNT)) rules"
 # Clean up
 rm -rf ${RULES_TAR} ${RULES_DIR} ${RULES_FILE}
 rm AUTHORS LICENSE VRT-License.txt
